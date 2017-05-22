d825bda67c30938bd19fccecf8008c2c3bf12a07
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-22 08:24:39 -0600

    More boilerplate

--- src/main/java/io/vertx/ext/prometheus/impl/VertxMetricsFactoryImpl.java
+++ src/main/java/io/vertx/ext/prometheus/impl/VertxMetricsFactoryImpl.java
@@ -1,7 +1,11 @@
 package io.vertx.ext.prometheus.impl;
 
+import io.prometheus.client.CollectorRegistry;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.impl.FileResolver;
+import io.vertx.core.json.DecodeException;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.metrics.MetricsOptions;
@@ -9,13 +13,48 @@ import io.vertx.core.spi.VertxMetricsFactory;
 import io.vertx.core.spi.metrics.VertxMetrics;
 import io.vertx.ext.prometheus.PrometheusMetricsOptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class VertxMetricsFactoryImpl implements VertxMetricsFactory {
   static final String BASE_NAME = "vertx";
   private Logger logger = LoggerFactory.getLogger(VertxMetricsFactoryImpl.class);
 
   @Override
-  public VertxMetrics metrics(Vertx vertx, VertxOptions vertxOptions) {
-    return null;
+  public VertxMetrics metrics(Vertx vertx, VertxOptions options) {
+    MetricsOptions baseOptions = options.getMetricsOptions();
+    PrometheusMetricsOptions metricsOptions;
+    if (baseOptions instanceof PrometheusMetricsOptions) {
+      metricsOptions = (PrometheusMetricsOptions) baseOptions;
+    } else {
+      metricsOptions = new PrometheusMetricsOptions(baseOptions.toJson());
+    }
+    // Check to see if a config file name has been set, and if it has load it and create new options file from it
+    if (metricsOptions.getConfigPath() != null && !metricsOptions.getConfigPath().isEmpty()) {
+      JsonObject loadedFromFile = loadOptionsFile(metricsOptions.getConfigPath(), new FileResolver(vertx));
+      if (!loadedFromFile.isEmpty()) {
+        metricsOptions = new PrometheusMetricsOptions(loadedFromFile);
+      }
+    }
+    String baseName = metricsOptions.getBaseName() == null ? BASE_NAME : metricsOptions.getBaseName();
+    return new VertxMetricsImpl(CollectorRegistry.defaultRegistry, options, metricsOptions, baseName);
+  }
+
+  private JsonObject loadOptionsFile(String configPath, FileResolver fileResolver) {
+    File file = fileResolver.resolveFile(configPath);
+    try (Scanner scanner = new Scanner(file)) {
+      scanner.useDelimiter("\\A");
+      String metricsConfigString = scanner.next();
+
+      return new JsonObject(metricsConfigString);
+    } catch (IOException ioe) {
+      logger.error("Error while reading metrics config file", ioe);
+    } catch (DecodeException de) {
+      logger.error("Error while decoding metrics config file into JSON", de);
+    }
+
+    return new JsonObject();
   }
 
   @Override
---
+++ src/main/java/io/vertx/ext/prometheus/impl/VertxMetricsImpl.java
@@ -0,0 +1,100 @@
+package io.vertx.ext.prometheus.impl;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+import io.vertx.core.Verticle;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.datagram.DatagramSocket;
+import io.vertx.core.datagram.DatagramSocketOptions;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.NetClientOptions;
+import io.vertx.core.net.NetServerOptions;
+import io.vertx.core.net.SocketAddress;
+import io.vertx.core.spi.metrics.*;
+import io.vertx.ext.prometheus.PrometheusMetricsOptions;
+
+public class VertxMetricsImpl implements VertxMetrics {
+  private static final String NAME_SEPARATOR = "_";
+  private final Gauge verticles;
+  private final Gauge timers;
+
+  public VertxMetricsImpl(CollectorRegistry registry, VertxOptions options, PrometheusMetricsOptions metricsOptions, String baseName) {
+    baseName += NAME_SEPARATOR;
+    verticles = Gauge.build(baseName + "verticles", "The number of verticles deployed").register(registry);
+    timers = Gauge.build(baseName + "timers", "The number of active timers").register(registry);
+  }
+
+  @Override
+  public void verticleDeployed(Verticle verticle) {
+    verticles.inc();
+  }
+
+  @Override
+  public void verticleUndeployed(Verticle verticle) {
+    verticles.dec();
+  }
+
+  @Override
+  public void timerCreated(long l) {
+    timers.inc();
+  }
+
+  @Override
+  public void timerEnded(long l, boolean b) {
+    timers.dec();
+  }
+
+  @Override
+  public EventBusMetrics createMetrics(EventBus eventBus) {
+    return null;
+  }
+
+  @Override
+  public HttpServerMetrics<?, ?, ?> createMetrics(HttpServer httpServer, SocketAddress socketAddress, HttpServerOptions httpServerOptions) {
+    return null;
+  }
+
+  @Override
+  public HttpClientMetrics<?, ?, ?, ?, ?> createMetrics(HttpClient httpClient, HttpClientOptions httpClientOptions) {
+    return null;
+  }
+
+  @Override
+  public TCPMetrics<?> createMetrics(SocketAddress socketAddress, NetServerOptions netServerOptions) {
+    return null;
+  }
+
+  @Override
+  public TCPMetrics<?> createMetrics(NetClientOptions netClientOptions) {
+    return null;
+  }
+
+  @Override
+  public DatagramSocketMetrics createMetrics(DatagramSocket datagramSocket, DatagramSocketOptions datagramSocketOptions) {
+    return null;
+  }
+
+  @Override
+  public <P> PoolMetrics<?> createMetrics(P p, String s, String s1, int i) {
+    return null;
+  }
+
+  @Override
+  public boolean isMetricsEnabled() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+
+  @Override
+  public void close() {
+
+  }
+}
